295ff2b13b274ecbfc4aff8f8876800450f89a76
Caroline Dussart <caroline.dussart@wanadoo.fr>
2019-11-30 19:52:36 +0100

    Proposition de tests + Ajout d'exceptions dans les classes

---
+++ Tests/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ClassLibraryDelta.Entities;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        [Test]
+        public void ClientCreatedWithoutLastName()
+        {
+            Client c = new Client();
+            Assert.IsNull(c);
+        }
+    }
+}
