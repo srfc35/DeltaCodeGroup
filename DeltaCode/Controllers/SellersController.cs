90628e20e6d7d10164c837490188755fae9362f1
Caroline Dussart <caroline.dussart@wanadoo.fr>
2019-12-29 15:51:10 +0100

    Ajout du controleur pour les vendeurs. Necessitera des casts Person -> Seller

---
+++ DeltaCode/Controllers/SellersController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClassLibraryDelta.Database;
+using ClassLibraryDelta.Entities;
+
+namespace DeltaCode.Controllers
+{
+    public class SellersController : Controller
+    {
+        private ProductContext db = new ProductContext();
+
+        // GET: Sellers
+        public ActionResult Index()
+        {
+            return View(db.Persons.ToList());
+        }
+
+        // GET: Sellers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seller seller = db.Persons.Find(id);
+            if (seller == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seller);
+        }
+
+        // GET: Sellers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sellers/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,LastName,FirstName,Phone,SellerAccount,Login,Password")] Seller seller)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Persons.Add(seller);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(seller);
+        }
+
+        // GET: Sellers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seller seller = db.Persons.Find(id);
+            if (seller == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seller);
+        }
+
+        // POST: Sellers/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,LastName,FirstName,Phone,SellerAccount,Login,Password")] Seller seller)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(seller).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(seller);
+        }
+
+        // GET: Sellers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seller seller = db.Persons.Find(id);
+            if (seller == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seller);
+        }
+
+        // POST: Sellers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Seller seller = db.Persons.Find(id);
+            db.Persons.Remove(seller);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
