b8bba631255e1d12ed0b33c1f14ff5a6d0fad4e8
Caroline Dussart <caroline.dussart@wanadoo.fr>
2020-01-11 11:56:24 +0100

    Renommer DeltaCode en ASP.NET

---
+++ ASP.NET/Models/ViewModels/AccountViewModels.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DeltaCode.Models
+{
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mot de passe")]
+        public string Password { get; set; }
+
+        [Display(Name = "Mémoriser le mot de passe ?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Nom d'utilisateur")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Role")]
+        public string Role { get; set; }
+
+        [Required]
+        [Display(Name = "Login utilisateur")]
+        public string Login { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "La chaîne {0} doit comporter au moins {2} caractères.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mot de passe")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmer le mot de passe ")]
+        [Compare("Password", ErrorMessage = "Le mot de passe et le mot de passe de confirmation ne correspondent pas.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Role")]
+        public string RoleId { get; set; }
+    }
+
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Courrier électronique")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "La chaîne {0} doit comporter au moins {2} caractères.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mot de passe")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmer le mot de passe")]
+        [Compare("Password", ErrorMessage = "Le nouveau mot de passe et le mot de passe de confirmation ne correspondent pas.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+
+    public class IdentityRoleListViewModel
+    {
+        public List<IdentityRole> Roles { get; set; }
+    }
+}
---
+++ ASP.NET/Utils/IdentityUtils/RoleUtils.cs
@@ -0,0 +1,52 @@
+﻿using DeltaCode.Models;
+using DeltaCode.Models.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeltaCode.Utils.IdentityUtils
+{
+    public static class RoleUtils
+    {
+        public static IdentityRole CreateOrGetRole(string roleName)
+        {
+            using (var ctx = new SecurityDbContext())
+            {
+                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(ctx));
+                IdentityRole result = null;
+
+                if (!roleManager.RoleExists(roleName))
+                {
+                    result = new IdentityRole();
+                    result.Name = roleName;
+                    roleManager.Create(result);
+                }
+
+                return result ?? roleManager.FindByName(roleName);
+            }
+        }
+
+        public static List<IdentityRole> GetRoles()
+        {
+            using (var ctx = new SecurityDbContext())
+            {
+                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(ctx));
+
+                return roleManager.Roles.ToList();
+            }
+        }
+
+        public static void AssignRoleToUser(IdentityRole role, MyIdentityUser user)
+        {
+            using (var ctx = new SecurityDbContext())
+            {
+                var userManager = new UserManager<MyIdentityUser>(new UserStore<MyIdentityUser>(ctx));
+
+                userManager.AddToRole(user.Id, role.Name);
+            }
+        }
+    }
+}
