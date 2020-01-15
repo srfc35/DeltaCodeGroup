fabf3e2621b84f32341378bf73b6788f7a1de8e8
Caroline Dussart <caroline.dussart@wanadoo.fr>
2020-01-15 13:28:21 +0100

    Ajout du panier dans la page TVs

--- UWP/ViewModels/TVPageViewModel.cs
+++ UWP/ViewModels/TVPageViewModel.cs
@@ -19,6 +19,8 @@ namespace UWP.ViewModels
         private INavigationService navigationService;
         private DatabaseService databaseService;
 
+        public float Amount { get; set; }
+
         public ObservableCollection<TV> TVs { get; set; }
 
         public TVPageViewModel(INavigationService navigationService, DatabaseService databaseService)
--- UWP/Views/Products/TVPage.xaml.cs
+++ UWP/Views/Products/TVPage.xaml.cs
@@ -21,13 +21,32 @@ namespace UWP.Views.Products
     /// <summary>
     /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
     /// </summary>
-    public sealed partial class TVPage : Page
+    public sealed partial class TVPage : Page, INavigationEvent
     {
         public TVPageViewModel ViewModel { get; set; }
 
         public TVPage()
         {
             this.InitializeComponent();
+        }
+
+        void INavigationEvent.OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            (this.DataContext as INavigationEvent).OnNavigatedTo(e);
+            if (ViewModel.Amount.Equals(null))
+            {
+                ViewModel.Amount = 0;
+            }
+            else
+            {
+                ViewModel.Amount = (float)e.Parameter;
+            }
+        }
+
+        void INavigationEvent.OnNavigatedFrom(NavigationEventArgs e)
+        {
+
         }
     }
 }
